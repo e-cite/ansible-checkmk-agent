d2ef9a188d45ff24987d26ae6143580e6797ef1d
Andreas Dolp <dev@andreas-dolp.de>
2023-01-07 23:53:30 +0100

    Fix: needrestart.sh korrekter Status auch bei UNKNW.

--- files/local/needrestart.sh
+++ files/local/needrestart.sh
@@ -2,16 +2,18 @@
 # -lkw -> libraries, kernel + CPU microcode
 # -p NAGIOS output - needs to be rearranged for checkmk and put in one line
 NEEDRESTART=$(sudo needrestart -lkwp | awk -v ORS='\\n' '1')
-STATE=$(echo $NEEDRESTART | awk '{print $1}') #echo $NEEDRESTART  | sed 's/WARN/1/' | sed 's/- \([^|]*\)\(\|.*;[0-9]\+\)/NEEDRESTART \2 \1/' | sed 's/\|\([^\s]\+\)
+RESULT=$(echo $NEEDRESTART | awk '{print $1}') #echo $NEEDRESTART  | sed 's/WARN/1/' | sed 's/- \([^|]*\)\(\|.*;[0-9]\+\)/NEEDRESTART \2 \1/' | sed 's/\|\([^\s]\+\)
 METRICS=$(echo $NEEDRESTART | sed 's/^[^|]*|\(.*;[0-9]\+\)\\n.*/\1/' | sed 's/ /|/g' )
 OUTPUT=$(echo $NEEDRESTART | sed 's/^.*- \([^|]*\)|.*;[0-9]\+\(\\n.*\)/\1\2/')
 
-if [ "$STATE" == "WARN" ];then
+if [ "$RESULT" == "WARN" ]; then
   STATE="1"
-elif [ "$STATE" == "CRIT" ];then
+elif [ "$RESULT" == "CRIT" ]; then
   STATE=2
-elif [ "$STATE" == "OK" ];then
+elif [ "$RESULT" == "OK" ]; then
   STATE=0
+else
+  STATE=3
 fi
 
 echo "$STATE NEEDRESTART $METRICS $OUTPUT"
